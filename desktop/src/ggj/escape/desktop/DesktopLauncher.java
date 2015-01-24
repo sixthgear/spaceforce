26885006ac661a248e9706768c773bddea897a7a
sixthgear <sixthgear@sixthgear.ca>
2015-01-23 20:37:48 -0800

    Initial import

---
+++ desktop/src/ggj/escape/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package ggj.escape.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ggj.escape.EscapeGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new EscapeGame(), config);
+	}
+}
